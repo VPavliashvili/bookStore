89c79cd9627b290f9a0cf9b8229b14f9f2e5c656
VPavliashvili <v_pavliashvili@yahoo.com>
2024-05-17 00:59:57 +0400

    group abstraction implemented

--- api/router/router.go
+++ api/router/router.go
@@ -1,66 +1,67 @@
 package router
 
 import (
-	system "booksapi/api/resource/system"
-	"booksapi/api/router/middlewares"
-	"booksapi/docs"
-	"fmt"
 	"net/http"
-
-	httpSwagger "github.com/swaggo/http-swagger"
 )
 
-type customMux struct {
-	*http.ServeMux
-	middlewares []func(http.Handler) http.Handler
+type Group struct {
+	*CustomMux
+	pattern string
+	action  func(this *Group) http.Handler
 }
 
-func (m *customMux) handle(pattern string, handler http.Handler) {
+type CustomMux struct {
+	*http.ServeMux
+	middlewares []func(http.Handler) http.Handler
+	groups      []*Group
+}
+
+func (m *CustomMux) HandleRoute(pattern string, handler http.Handler) {
 	for _, middleware := range m.middlewares {
 		handler = middleware(handler)
 	}
 	m.Handle(pattern, handler)
 }
 
-func (m *customMux) handleFunc(pattern string, handler http.HandlerFunc) {
-	m.handle(pattern, handler)
+func (m *CustomMux) HandleRouteFunc(pattern string, handler http.HandlerFunc) {
+	m.HandleRoute(pattern, handler)
 }
 
-func (m *customMux) use(middlewares ...func(http.Handler) http.Handler) *customMux {
+func (m *CustomMux) Use(middlewares ...func(http.Handler) http.Handler) *CustomMux {
 	m.middlewares = append(m.middlewares, middlewares...)
 	return m
 }
 
-func New(systemApi system.API) *customMux {
-
-	docs.SwaggerInfo.Title = "Books store API"
-	docs.SwaggerInfo.Description = "This is a simple CRUD api implementation for educatinal purposes"
-	docs.SwaggerInfo.Version = "1.0"
-
-	router := &customMux{
-		ServeMux: http.NewServeMux(),
+func (m *CustomMux) AddGroup(pattern string, setup func(ng *Group)) {
+	newGroup := &Group{
+		CustomMux: &CustomMux{
+			ServeMux: http.NewServeMux(),
+		},
+		pattern: pattern,
 	}
-	router.use(middlewares.ContentTypeJSON)
-	router.handle("/api/system/", systemRouteGroup(systemApi))
 
-	router.HandleFunc("GET /swagger/*", httpSwagger.Handler(
-		httpSwagger.URL(fmt.Sprintf("http://localhost:%d/swagger/doc.json", 6012)),
-	))
+	newGroup.action = func(ng *Group) http.Handler {
+		setup(ng)
 
-	return router
+		// /api/group/ should become /api/group therefor stripping before returning
+		// this way we will get "/api/group/endpoint" instead of "/api/group//endpoint"
+		return http.StripPrefix(pattern[:len(pattern)-1], ng)
+	}
+
+	m.groups = append(m.groups, newGroup)
 }
 
-func systemRouteGroup(api system.API) http.Handler {
-	subRouter := &customMux{
+func CreateAndSetup(setup func(this *CustomMux) *CustomMux) *CustomMux {
+	mux := &CustomMux{
 		ServeMux: http.NewServeMux(),
 	}
 
-	subRouter.handleFunc("GET /health", func(w http.ResponseWriter, r *http.Request) {
-		api.HandleHealth(w, r)
-	})
-	subRouter.handleFunc("GET /about", func(w http.ResponseWriter, r *http.Request) {
-		api.HandleAbout(w, r)
-	})
+	mux = setup(mux)
 
-	return http.StripPrefix("/api/system", subRouter)
+	for _, g := range mux.groups {
+		h := g.action(g)
+		mux.HandleRoute(g.pattern, h)
+	}
+
+	return mux
 }
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,19 +1,46 @@
 package main
 
 import (
-	system "booksapi/api/resource/system"
+	"booksapi/api/resource/system"
 	"booksapi/api/router"
+
+	"booksapi/api/router/middlewares"
+	"booksapi/docs"
 	"fmt"
 	"net/http"
 	"time"
+
+	httpSwagger "github.com/swaggo/http-swagger"
 )
 
 func main() {
-	buildTime := time.Now().String() // TO DO
 
+	docs.SwaggerInfo.Title = "Books store API"
+	docs.SwaggerInfo.Description = "This is a simple CRUD api implementation for educatinal purposes"
+	docs.SwaggerInfo.Version = "1.0"
+
+	buildTime := time.Now().String() // TO DO
 	systemApi := system.New(buildTime)
 
-	router := router.New(systemApi)
+	router := router.CreateAndSetup(func(this *router.CustomMux) *router.CustomMux {
+		this.Use(middlewares.ContentTypeJSON)
+
+		this.AddGroup("/api/system/", func(ng *router.Group) {
+			ng.HandleRouteFunc("GET /health", func(w http.ResponseWriter, r *http.Request) {
+				systemApi.HandleHealth(w, r)
+			})
+			ng.HandleRouteFunc("GET /about", func(w http.ResponseWriter, r *http.Request) {
+				systemApi.HandleAbout(w, r)
+			})
+		})
+
+		this.HandleFunc("GET /swagger/*", httpSwagger.Handler(
+			httpSwagger.URL(fmt.Sprintf("http://localhost:%d/swagger/doc.json", 6012)),
+		))
+
+		return this
+
+	})
 
 	server := &http.Server{
 		Addr:    fmt.Sprintf(":%d", 6012),
